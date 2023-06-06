b854239fc4d6fbe4f567d8c67fa88b6a9e9f60e5
emostalooknegativeqi <emostalook@gmail.com>
2023-06-06 08:29:10 -0300

    one and one and one is three

--- balao/Main.java
+++ balao/Main.java
@@ -1,5 +1,10 @@
 package balao;
 
 public class Main {
-
+    public static void main(String[]args){
+        //construir baloes
+        Balao b1 = new Balao();
+        String resultado = b1.voar();
+        System.out.println(resultado);
+    }
 }
