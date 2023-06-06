686b1884e40776413ff5c9d59ffb0f411fc57d1f
emostalooknegativeqi <emostalook@gmail.com>
2023-06-06 08:26:02 -0300

    d4c

--- balao/Balao.java
+++ balao/Balao.java
@@ -1,4 +1,18 @@
 package balao;
 
 public class Balao {
+    //atributos
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+
+    //metodos
+    public String voar(){
+        return "voando";
+    }
 }
